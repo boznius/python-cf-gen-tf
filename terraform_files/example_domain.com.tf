5576810c12bba257bbe8f1fca91265ea8f553a44
boznius <bbozhev@gmail.com>
2024-01-24 18:44:57 +0200

    adding example tf domain records

--- terraform_files/example_domain.com.tf
+++ terraform_files/example_domain.com.tf
@@ -1,1 +1,22 @@
 // example terraform domain file for records to be put into.
+resource "cloudflare_zone" "example_domain.com" {
+  zone = "example_domain.com"
+}
+
+resource "cloudflare_record" "example_domain.com_email.example_domain.com_CNAME" {
+  zone_id = cloudflare_zone.example_domain.com.id
+  name    = "email.example_domain.com"
+  type    = "CNAME"
+  value   = "eu.mailgun.org"
+  proxied = false
+}
+
+resource "cloudflare_record" "example_domain.com_example_domain.com_CNAME" {
+  zone_id = cloudflare_zone.example_domain.com.id
+  name    = "example_domain.com"
+  type    = "CNAME"
+  value   = "obscure-tangerine.herokudns.com"
+  proxied = false
+}
+
+
